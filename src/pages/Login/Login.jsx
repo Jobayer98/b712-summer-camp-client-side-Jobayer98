faa4ab3f71cded48b70a008555ce0b57a0f603a1
Jobayer <jobayerrahman0220@gmail.com>
2023-06-08 02:33:18 +0600

    add login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,42 @@
+import { useForm } from "react-hook-form";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            {...register("email", { required: true })}
+          />
+          {/* {errors.email && <span>Invalid email</span>} */}
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            placeholder="Enter your password"
+            {...register("password", { required: true })}
+          />
+          {/* {errors.password && <span>Invalid password</span>} */}
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <div>
+        <button>Login with google</button>
+        <button>Login with facebook</button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
