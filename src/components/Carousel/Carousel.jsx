80cad5d080453449b521936fa76bd82e1bb2eb00
Jobayer <jobayerrahman0220@gmail.com>
2023-06-08 22:46:43 +0600

    add carousel

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,47 @@
+import { Autoplay, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./styles.css";
+import img_1 from "../../assets/banner_1.jpg";
+import img_2 from "../../assets/banner_2.jpg";
+import img_3 from "../../assets/banner_3.jpg";
+import img_4 from "../../assets/banner_4.jpg";
+import img_5 from "../../assets/banner_5.jpg";
+
+const Carousel = () => {
+  return (
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 7000,
+        disableOnInteraction: false,
+      }}
+      navigation={true}
+      modules={[Autoplay, Navigation]}
+      className="mySwiper "
+    >
+      <SwiperSlide>
+        <img src={img_1} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img_2} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img_3} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img_4} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img_5} alt="" />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Carousel;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,10 @@
+import Carousel from "../../components/Carousel/Carousel";
+import Container from "../../components/shared/Container";
 const HomePage = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et praesentium
-      eligendi nesciunt quod asperiores suscipit nulla necessitatibus,
-      doloremque soluta odio voluptates deserunt ullam consequuntur. Eligendi
-      natus harum, reprehenderit quod repudiandae mollitia eaque in ullam ex
-      minima impedit amet commodi nemo accusamus assumenda rem hic expedita
-      molestias saepe ipsum sed omnis facilis nam? Illum pariatur, error sunt
-      nisi, impedit rem rerum id molestias architecto a ut dolores eius. Vel
-      atque voluptatem dolores fugiat earum. Et omnis explicabo soluta tempore.
-      Rerum, nostrum sint maxime nihil inventore voluptatum quam fuga reiciendis
-      sunt illo illum minus laborum praesentium exercitationem veritatis
-      perferendis deleniti! Blanditiis, neque!
-    </div>
+    <Container>
+      <Carousel />
+    </Container>
   );
 };
 
