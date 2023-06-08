adeccd93b56a2ecd7d3cacaf6689c740a306a0be
Jobayer <jobayerrahman0220@gmail.com>
2023-06-09 02:14:58 +0600

    add aos animation

--- src/components/UI/Card/Card.jsx
+++ src/components/UI/Card/Card.jsx
@@ -1,6 +1,13 @@
+import Aos from "aos";
+import { useEffect } from "react";
 const Card = ({ img, lang, prog }) => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+    });
+  }, []);
   return (
-    <div className="border w-full shadow">
+    <div className="border w-full shadow" data-aos="fade-up">
       <figure className="flex justify-center">
         <img className="w-96" src={img} alt="card_img" />
       </figure>
